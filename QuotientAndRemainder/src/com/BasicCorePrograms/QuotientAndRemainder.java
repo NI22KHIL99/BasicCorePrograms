636a70a5138dde3e2632c2c56666732ce8ed1fac
NI22KHIL99 <nikhilkumbhar.2299@gmail.com>
2021-09-28 04:03:19 +0530

    Added: QuotientAndRemainder

---
+++ QuotientAndRemainder/src/com/BasicCorePrograms/QuotientAndRemainder.java
@@ -0,0 +1,17 @@
+package com.BasicCorePrograms;
+
+public class QuotientAndRemainder {
+	
+	public static void main(String[] args)
+    {
+  
+        int dividend = 345, divisor = 7;
+  
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+  
+        System.out.println("The Quotient is = " + quotient);
+        System.out.println("The Remainder is = " + remainder);
+    }
+
+}
